c7f773e499af1bd3e600ad2cb7bdff95df75e353
Abby Brown <abby_brown3@baylor.edu>
2021-03-02 22:22:05 -0600

    Done for the night; sets next. All code function

--- dictionaries!!.py
+++ dictionaries!!.py
@@ -90,3 +90,33 @@ print(mydict_copy)
 
 mydict_copy["email"] = "max@123.com"
 print(mydict_copy, mydict)
+
+# Updating a dictionary (39:15)
+my_dict = {"name": "Max", "age": 56, "email": "eww@eww.com"}
+my_otherdict = dict(name="Martha", age=102, city="Hotlanta")
+
+my_dict.update(my_otherdict)
+print(my_dict)
+
+# This is FASCINATING! All existing key pairs were overwritten; email was NOT becuase it was not an item in my_otherdict
+
+# Possible key types
+##Can use any immutable type
+###Can even use a tuple???
+
+my_dict = {3: 9, 6: 36, 9: 81}
+print(my_dict)
+
+# value = my_dict[0]
+# Error! Zero isn't in our list and it's not an index.  Use the actual key to access.
+
+value = my_dict[3]
+print(value)  # This will return the value 9!
+
+# Use a tuple as a key
+mytuple = (8, 7)
+my_dict = {mytuple: 15}
+
+print(my_dict)
+# Tuples are possible, but a list would throw an exception
+# Lists are mutable and can be changed; so it's not hashable and can't be used as a key
